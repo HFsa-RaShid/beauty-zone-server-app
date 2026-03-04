07fc06f47cc1de149b8e3b03a47deaae09a4b6c2
HFsa-RaShid <hafsa20cse028@gmail.com>
2026-03-04 13:13:30 +0600

    update

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
   origin: [
         'http://127.0.0.1:5500', 
         'http://localhost:5500', 
-        'https://beautyzone-client-eta.vercel.app' 
+        'https://beauty-zone-clientapp.vercel.app' 
     ],
     credentials: true
 }));
